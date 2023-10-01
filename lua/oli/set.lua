96402e7b7454c8df90a86f6e16bb845228c88d99
Oliver Green <ojgreensemail@gmail.com>
2023-10-01 22:32:07 +0100

    Fixed error on windows

--- lua/oli/set.lua
+++ lua/oli/set.lua
@@ -12,7 +12,14 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- Configure undodir depending on OS
+if os.getenv("HOME") then
+    vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+else 
+    vim.opt.undodir = os.getenv("USERPROFILE") .. "/.vim/undodir"
+end
+
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
