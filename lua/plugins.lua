c3d67b7a06b56e0384b4addee13ad155257d5f0c
Oliver Green <ojgreensemail@gmail.com>
2024-03-01 12:08:20 +0000

    Added vim abolish

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 local packer_bootstrap = ensure_packer()
 
@@ -67,6 +67,9 @@ return require('packer').startup(function()
     use('mbbill/undotree')
     --- Git integration
     use('tpope/vim-fugitive')
+    --- Vim Abolish
+    --- Replaces words with similar words, easily (use S instead of s during substitution)
+    use('tpope/vim-abolish')
     --- Surround text objects
     use('machakann/vim-sandwich')
     --- AI Copilot
