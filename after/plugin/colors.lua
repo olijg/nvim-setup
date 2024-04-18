de37b24fa1b138414757119d139bc38483c979c8
Oliver Green <ojgreensemail@gmail.com>
2024-04-18 16:11:18 +0100

    Changing highlight colour

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -4,6 +4,8 @@ function ColorMyPencils(color)
     --- Make the Background transparent
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- Highlight Colours
+    vim.cmd("highlight Visual ctermbg=green ctermfg=black guibg=green guifg=blue" )
 end
 
 ColorMyPencils()
