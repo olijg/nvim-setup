5174abb0cb72b81c5fbf1c7a327b5a3d1fd95fc1
Oli <ojgreensemail@gmail.com>
2024-04-19 12:27:17 +0100

    Fixing deprecation, signcolumn -> auto

--- lua/opts.lua
+++ lua/opts.lua
@@ -7,13 +7,13 @@
 -- updatetime: set updatetime for CursorHold
 vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
 vim.opt.shortmess = vim.opt.shortmess + { c = true }
-vim.api.nvim_set_option('updatetime', 300)
+vim.api.nvim_set_option_value('updatetime', 300, {scope = 'global'})
 
 -- Fixed column for diagnostics to appear
 -- Show autodiagnostic popup on cursor hover_range
 -- Goto previous / next diagnostic warning / error
 -- Show inlay_hints more frequently
 vim.cmd([[
-set signcolumn=yes
+set signcolumn=auto
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
 ]])
