11f6a915057728c034d3642155c096b12b8893a1
Oli <ojgreensemail@gmail.com>
2024-05-29 09:08:21 +0100

    Reduce buffer scroll

--- lua/oli/set.lua
+++ lua/oli/set.lua
@@ -25,7 +25,7 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 6
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
