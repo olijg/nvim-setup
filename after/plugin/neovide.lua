c8d069a7ac38f38f2de7c8fdb113c6352e7035e7
Oliver Green <ojgreensemail@gmail.com>
2025-02-04 11:43:44 +0000

    Removed superfluous blur

--- after/plugin/neovide.lua
+++ after/plugin/neovide.lua
@@ -2,5 +2,4 @@ if vim.g.neovide then
   vim.o.guifont = "JetBrainsMono Nerd Font:h12"
   vim.g.neovide_transparency = 0.9
   vim.g.neovide_normal_opacity = 0.9
-  vim.g.neovide_window_blurred = true
 end
