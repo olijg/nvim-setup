5103e6e603d31d59c79ff519a6b52e20ec31b3d3
Oli <ojgreensemail@gmail.com>
2025-02-07 12:00:00 +0000

    Just one working command

--- ftplugin/http.lua
+++ ftplugin/http.lua
@@ -5,18 +5,3 @@ vim.api.nvim_buf_set_keymap(
   "<cmd>lua require('kulala').run()<cr>",
   { noremap = true, silent = true, desc = "Execute the request" }
 )
-
-vim.api.nvim_buf_set_keymap(
-    0,
-    "n",
-    "[",
-    "<cmd>lua require('kulala').jump_prev()<cr>",
-    { noremap = true, silent = true, desc = "Jump to the previous request" }
-)
-vim.api.nvim_buf_set_keymap(
-    0,
-    "n",
-    "]",
-    "<cmd>lua require('kulala').jump_next()<cr>",
-    { noremap = true, silent = true, desc = "Jump to the next request" }
-)
