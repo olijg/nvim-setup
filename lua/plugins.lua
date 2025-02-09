ebbd247dd4478b1d01eebdb36d2ae4b1b5468769
Oli <ojgreensemail@gmail.com>
2025-02-09 12:36:33 +0000

    Changed dashboard provider

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -25,14 +25,26 @@ require("lazy").setup({
     end
   },
 
-  -- Dashboard
+  -- Snacks
   {
-    'echasnovski/mini.starter',
-    version = false,
+    "folke/snacks.nvim",
+    priority = 1000,
     lazy = false,
-    config = function()
-      require('mini.starter').setup()
-    end
+    ---@type snacks.Config
+    opts = {
+      bigfile = { enabled = false },
+      dashboard = { enabled = true },
+      explorer = { enabled = false },
+      indent = { enabled = false },
+      input = { enabled = false },
+      picker = { enabled = false },
+      notifier = { enabled = false },
+      quickfile = { enabled = false },
+      scope = { enabled = false },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = false },
+      words = { enabled = false },
+    },
   },
 
   -- Language Server Installer
