27bbcc07d0cd7a246dce2b9e8e0e887ba0ab12e9
Oli <ojgreensemail@gmail.com>
2025-02-09 14:20:01 +0000

    Fixing colours hopefully

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,6 @@
 function ColorMyPencils(color)
+  vim.opt.termguicolors = true
   color = color or "molokai"
-  --vim.cmd.colorscheme(color)
-  --- Make the Background transparent
-  -- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  -- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   -- Custom Colours
   vim.cmd("highlight Visual ctermbg=blue ctermfg=green guibg=darkcyan guifg=chartreuse")
   vim.cmd("highlight CmpItemMenuDefault guifg=#ffe792")
