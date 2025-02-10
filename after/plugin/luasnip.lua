99af74cab1541a6fc49d11699df87e7d6d624ad4
Oli <ojgreensemail@gmail.com>
2025-02-10 14:03:51 +0000

    Removing lua snippet leftovers

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,4 +1,3 @@
-local config_dir = vim.fn.stdpath('config')
 local ls = require("luasnip")
 
 vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
@@ -12,8 +11,3 @@ vim.keymap.set({"i", "s"}, "<C-E>", function()
 end, {silent = true})
 
 require("luasnip.loaders.from_vscode").lazy_load()
--- require("luasnip.loaders.from_lua").load(
---   {
---     paths = { config_dir .. "./lua/snippets/" }
---   }
--- )
