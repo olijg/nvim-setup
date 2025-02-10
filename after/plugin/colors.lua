9f9fa03fa783fd8a3b1084bc280472cc619f5619
Oli <ojgreensemail@gmail.com>
2025-02-10 02:15:05 +0000

    Removing unnecessary colours

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,13 +1,6 @@
 function ColorMyPencils(color)
   vim.opt.termguicolors = true
   color = color or "molokai"
-  -- Custom Colours
-  vim.cmd("highlight Visual ctermbg=blue ctermfg=green guibg=darkcyan guifg=chartreuse")
-  vim.cmd("highlight CmpItemMenuDefault guifg=#ffe792")
-  vim.cmd("highlight CmpItemAbbrMatchDefault guifg=#ffe792")
-  vim.cmd("highlight CmpItemKindDefault guifg=#ffe792")
-  vim.cmd("highlight CmpItemAbbrDefault guifg=#ffe792")
-  vim.cmd("highlight CmpItemAbbrMatchFuzzyDefault guifg=#ffe792")
 end
 
 ColorMyPencils()
