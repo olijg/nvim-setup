e6f0727b906559a0cc66f60f24fe4161209297a4
Oli <ojgreensemail@gmail.com>
2025-02-10 02:03:26 +0000

    More useful dashboard

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -33,7 +33,15 @@ require("lazy").setup({
     ---@type snacks.Config
     opts = {
       bigfile = { enabled = false },
-      dashboard = { enabled = true },
+      dashboard = { enabled = true,
+        sections = {
+          { section = "header" },
+          { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+          { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          { section = "startup" },
+        }
+      },
       explorer = { enabled = false },
       indent = { enabled = false },
       input = { enabled = false },
