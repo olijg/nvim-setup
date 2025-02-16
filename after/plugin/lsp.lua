71fbaf4bc18084c1c876a43b7132ea0c7f60bd9d
Oli <ojgreensemail@gmail.com>
2025-02-16 15:59:30 +0000

    Removed conflicting bindings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -110,8 +110,6 @@ cmp.setup({
     ['<Tab>'] = cmp.mapping.select_next_item(),
     ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
-    ['<C-j>'] = cmp.mapping.select_next_item(),
     ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
