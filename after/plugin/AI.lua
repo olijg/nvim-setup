f8da016e14b0df22036d67d68b70df1207a5b6e2
Oli <ojgreensemail@gmail.com>
2025-02-21 14:01:49 +0000

    Removing commented line

--- after/plugin/AI.lua
+++ after/plugin/AI.lua
@@ -18,7 +18,7 @@ require("codecompanion").setup({
   },
 })
 -- Code Companion
---
+
 vim.api.nvim_set_keymap("n", "<C-g>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<C-g>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>gc", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
