686c2ce492d65edaac72a384752cf8a06ac4e229
Oli <ojgreensemail@gmail.com>
2025-02-25 09:58:14 +0000

    Split style git

--- after/plugin/neogit.lua
+++ after/plugin/neogit.lua
@@ -1,2 +1,9 @@
 local neogit = require('neogit')
-vim.keymap.set("n", "<leader>gs", function() neogit.open() end)
+
+vim.keymap.set("n", "<leader>gs", function()
+  neogit.open({ kind = "split_above_all" })
+end)
+
+vim.keymap.set("n", "<leader>gg", function()
+  neogit.open()
+end)
