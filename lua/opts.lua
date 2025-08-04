8f27c102f5110afe279d96ea2aa910a3f059667e
Oli <ojgreensemail@gmail.com>
2025-08-04 12:05:34 +0100

    Removing completeopt in favour of blink defaults

--- lua/opts.lua
+++ lua/opts.lua
@@ -1,11 +1,13 @@
---Set completeopt to have a better completion experience
+-- Set completeopt to have a better completion experience 
+-- (Deprecated: not sure I need this with blink?)
 -- :help completeopt
+
 -- menuone: popup even when there's only one match
 -- noinsert: Do not insert text until a selection is made
 -- noselect: Do not select, force to select one from the menu
+
 -- shortmess: avoid showing extra messages when using completion
 -- updatetime: set updatetime for CursorHold
-vim.opt.completeopt = { 'menuone', 'noselect', 'noinsert' }
 vim.opt.shortmess = vim.opt.shortmess + { c = true }
 vim.api.nvim_set_option_value('updatetime', 300, { scope = 'global' })
 
