975fa15f45a94435f5e3166ebd5358473e6aa087
Oliver Green <oliver@OliPad.localdomain>
2025-11-10 11:36:33 +0000

    remove <C-k> mapping

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,6 +1,7 @@
 local ls = require("luasnip")
 
-vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+--TODO!: Decide if these are necessary
+-- vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
 vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
 vim.keymap.set({"i", "s"}, "<C-H>", function() ls.jump(-1) end, {silent = true})
 
